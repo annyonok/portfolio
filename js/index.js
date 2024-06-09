765dd0680a8808996f80e942439b7d3a9a77d5f8
annyonok <annyonok@gmail.com>
2024-06-09 19:38:36 +0300

    add js

---
+++ js/index.js
@@ -0,0 +1,39 @@
+new Swiper('.swiper', {
+    navigation: {
+        nextEl: '.portfolio__arrow_right',
+        prevEl: '.portfolio__arrow_left',
+        disabledClass: 'portfolio__arrow_disable',
+      },
+      // Responsive breakpoints
+    breakpoints: {
+    // when window width is >= 480px
+    480: {
+      grid: {
+        fill: 'row',
+        rows: 2
+      }
+    },
+    // when window width is >= 768px
+    768: {
+        slidesPerView: 2,
+        grid: {
+            fill: 'row',
+            rows: 2
+          }        
+        }
+  }
+});
+
+// Menu
+$('.header__contacts-burger').click(() => {
+    $('.header__contacts').slideToggle().css('display', 'flex');
+})
+
+// Modal
+$('.present__order-btn').click(() => {
+    $('.page__overlay_modal').fadeIn(400);
+})
+
+$('.modal__close').click(() => {
+    $('.page__overlay_modal').fadeOut(400);
+})
